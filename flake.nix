e210d96c0a68d9b629aa20f0f9277f3de5ce896a
Lawrence Chonavel <95857153+lawcho@users.noreply.github.com>
2026-08-05 11:47:53 +0200

    Bump Agda pin
    To a version with fewer bugs

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/24.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.agda.url = "github:agda/agda/master";
+  inputs.agda.url = "github:agda/agda/59f6359"; # Older versions have JS backend bugs
   outputs = { self, nixpkgs, flake-utils, agda }:
     (flake-utils.lib.eachDefaultSystem (system: let
         pkgs = nixpkgs.legacyPackages.${system};

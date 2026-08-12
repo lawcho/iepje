150490d8fb48673535250d57206d79c6950a8600
Lawrence Chonavel <95857153+lawcho@users.noreply.github.com>
2026-08-12 13:01:24 +0200

    Unburden CI by separating dev-only dependencies

--- flake.nix
+++ flake.nix
@@ -3,27 +3,33 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.agda.url = "github:agda/agda/59f6359"; # Older versions have JS backend bugs
   outputs = { self, nixpkgs, flake-utils, agda }:
-    (flake-utils.lib.eachDefaultSystem (system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        # Entry point for `nix build` and `nix develop`
-        packages.default = pkgs.stdenv.mkDerivation {
-          name = "iepje-examples";
-          src = ./.;
-          buildInputs = [
-            pkgs.darkhttpd
-            pkgs.inotify-tools
-            pkgs.xdg-utils
-            agda.outputs.packages.${system}.default.bin
-            ];
-          buildPhase = ''
-            bash compile.sh;
-            '';
-          installPhase = ''
-            mkdir $out
-            cp -r web/. $out
-          '';
-        };
-      }
-    ));
+    (flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          pkg = pkgs.stdenv.mkDerivation  # For building the examples gallery
+            {
+              name = "iepje-examples";
+              src = ./.;
+              buildInputs = [ agda.outputs.packages.${system}.default.bin ];
+              buildPhase = ''
+                bash compile.sh;
+                '';
+              installPhase = ''
+                mkdir $out
+                cp -r web/. $out
+              '';
+            };
+          devShell = pkgs.mkShell # For developing Iepje
+            {
+              packages = [pkgs.darkhttpd pkgs.inotify-tools pkgs.xdg-utils];
+              inputsFrom = [ pkg ];
+            };
+        in
+        {
+          packages.default = pkg;
+          devShells.default = devShell;
+        }
+      )
+    );
 }
